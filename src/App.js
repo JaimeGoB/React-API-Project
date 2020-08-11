18912cacdf7610ed59d7def5f9cc31a9f643f93f
JaimeGoB <josba93@gmail.com>
2020-08-10 21:50:03 -0500

    Added randome users API and outputting them to browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentWillMount() {
+    axios('https://api.randomuser.me/?nat=US&results=5')
+      .then(response => this.setState({
+        users: response.data.results
+      }))
+  }
+
   render() {
     return (
       <div className="App" >
-        <h1>Hello World!</h1>
+        {this.state.users.map(user =>
+          <div>
+            <h3>{user.cell}</h3>
+            <p>{user.name.first} {user.name.last}</p>
+            <hr />
+          </div>)}
       </div>
     );
   }
